f7b32acb511c538aa455526c4a22b8cb253ee8e3
York Lin <superj80820@gmail.com>
2025-04-01 16:36:48 +0800

    refactor: remove static export and image optimization settings from Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // 啟用靜態輸出
-  basePath: '/piano-practice',  // 設定基礎路徑為倉庫名稱
-  images: {
-    unoptimized: true,  // GitHub Pages 不支援 Next.js 的圖片優化
-  },
 }
 
 module.exports = nextConfig 
